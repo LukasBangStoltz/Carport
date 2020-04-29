f70fc0cf987b43c58ecbcd6dd6b6dfb97f7c7236
= <cph-cw109@cphbusiness.dk>
2020-04-29 14:54:05 +0200

    Nu med login og register funktion

--- Projektskabelon-master/src/main/java/DBAccess/UserMapper.java
+++ Projektskabelon-master/src/main/java/DBAccess/UserMapper.java
@@ -2,6 +2,7 @@ package DBAccess;
 
 import FunctionLayer.LoginSampleException;
 import FunctionLayer.User;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,49 +10,60 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- The purpose of UserMapper is to...
-
- @author kasper
+ * The purpose of UserMapper is to...
+ *
+ * @author kasper
  */
 public class UserMapper {
 
-    public static void createUser( User user ) throws LoginSampleException {
+    public static void createUser(User user) throws LoginSampleException {
         try {
             Connection con = Connector.connection();
-            String SQL = "INSERT INTO Users (email, password, role) VALUES (?, ?, ?)";
-            PreparedStatement ps = con.prepareStatement( SQL, Statement.RETURN_GENERATED_KEYS );
-            ps.setString( 1, user.getEmail() );
-            ps.setString( 2, user.getPassword() );
-            ps.setString( 3, user.getRole() );
+            String SQL = "INSERT INTO bruger (navn, adresse, telefonNr, email, password, hjemby, role) VALUES (?, ?, ?,?,?,?,?)";
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getAdress());
+            ps.setString(3, user.getPhoneNumber());
+            ps.setString(4, user.getEmail());
+            ps.setString(5, user.getCity());
+            ps.setString(6, user.getPassword());
+            ps.setString(7, user.getRole());
+
             ps.executeUpdate();
             ResultSet ids = ps.getGeneratedKeys();
             ids.next();
-            int id = ids.getInt( 1 );
-            user.setId( id );
-        } catch ( SQLException | ClassNotFoundException ex ) {
-            throw new LoginSampleException( ex.getMessage() );
+            int id = ids.getInt(1);
+            user.setId(id);
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            throw new LoginSampleException(ex.getMessage());
         }
     }
 
-    public static User login( String email, String password ) throws LoginSampleException {
+    public static User login(String email, String password) throws LoginSampleException {
         try {
             Connection con = Connector.connection();
-            String SQL = "SELECT bruger_id, role FROM bruger "
+            String SQL = "SELECT * FROM bruger "
                     + "WHERE email=? AND password=?";
-            PreparedStatement ps = con.prepareStatement( SQL );
-            ps.setString( 1, email );
-            ps.setString( 2, password );
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ps.setString(1, email);
+            ps.setString(2, password);
+
             ResultSet rs = ps.executeQuery();
-            if ( rs.next() ) {
-                String role = rs.getString( "role" );
-                int id = rs.getInt( "bruger_id" );
-                User user = new User( email, password, role );
-                user.setId( id );
+            if (rs.next()) {
+                String role = rs.getString("role");
+                int id = rs.getInt("bruger_id");
+                String name = rs.getString("navn");
+                String adress = rs.getString("adresse");
+                String phoneNumber = rs.getString("telefonNr");
+                String city = rs.getString("by");
+                User user = new User(name,adress,phoneNumber,email,password,city,role);
+                user.setId(id);
                 return user;
             } else {
-                throw new LoginSampleException( "Could not validate user" );
+                throw new LoginSampleException("Could not validate user");
             }
-        } catch ( ClassNotFoundException | SQLException ex ) {
+        } catch (ClassNotFoundException | SQLException ex) {
             throw new LoginSampleException(ex.getMessage());
         }
     }

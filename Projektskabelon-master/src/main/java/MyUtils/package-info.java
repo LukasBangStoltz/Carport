9104213dbc9a30469e333f58ea024fb287379d4c
= <cph-cw109@cphbusiness.dk>
2020-05-20 13:27:56 +0200

    CalcJavaDoc

---
+++ Projektskabelon-master/src/main/java/MyUtils/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * MyUtils indeholder alle hjælpefunktionerne, som er essentielle for opgavens helhed.
+ * Udregnings klasse, Initializer klasse, Svg genererings klasse og validerings klasse.
+ */
+package MyUtils;
